33bdfbf99df2a00a32c69521c230f4670a619ac8
Markus Bachmann <info@bachi.biz>
2012-08-29 19:46:26 +0200

    Make dumper's available for sf2

---
+++ Tests/DumperManagerTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Bazinga\Bundle\GeocoderBundle\Tests;
+
+use Bazinga\Bundle\GeocoderBundle\DumperManager;
+
+/**
+ * @author Markus Bachmann <markus.bachmann@bachi.biz>
+ */
+class DumperManagerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var DumperManager
+     */
+    private $manager;
+
+    protected function setup()
+    {
+        $this->manager = new DumperManager;
+    }
+
+    public function testSet()
+    {
+        $this->assertNull($this->manager->set('test', $this->getMock('Geocoder\\Dumper\\DumperInterface')));
+    }
+
+    public function testGet()
+    {
+        $dumper = $this->getMock('Geocoder\\Dumper\\DumperInterface');
+        $this->manager->set('test', $dumper);
+
+        $this->assertEquals($dumper, $this->manager->get('test'));
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testGetNotExistDumper()
+    {
+        $this->manager->get('not-exist');
+    }
+
+    public function testHas()
+    {
+        $this->assertFalse($this->manager->has('test'));
+        $this->manager->set('test', $this->getMock('Geocoder\\Dumper\\DumperInterface'));
+        $this->assertTrue($this->manager->has('test'));
+    }
+
+    public function testRemove()
+    {
+        $this->manager->set('test', $this->getMock('Geocoder\\Dumper\\DumperInterface'));
+        $this->manager->remove('test');
+        $this->assertFalse($this->manager->has('test'));
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testRemoveNotExistDumper()
+    {
+        $this->manager->remove('not-exist');
+    }
+}
