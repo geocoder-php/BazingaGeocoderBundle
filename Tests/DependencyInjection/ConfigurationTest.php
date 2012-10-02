328d71fe77444422d285bf00f94de1c94aeefc98
Markus Bachmann <info@bachi.biz>
2012-10-02 19:10:55 +0200

    Add basic testcase for the configtree

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Bazinga\Bundle\GeocoderBundle\Tests\DependencyInjection;
+
+use Bazinga\Bundle\GeocoderBundle\DependencyInjection\Configuration;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * @author Markus Bachmann <markus.bachmann@bachi.biz>
+ */
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetConfigTreeBuilder()
+    {
+        $config = Yaml::parse(file_get_contents(__DIR__.'/Fixtures/config.yml'));
+
+        $configuration = new Configuration();
+        $treeBuilder = $configuration->getConfigTreeBuilder();
+        $processor = new Processor;
+
+        $config = $processor->process($treeBuilder->buildTree(), $config);
+    }
+}
