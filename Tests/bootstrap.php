89ae5852bbf75fce95a91430eb0fc9b73a361335
Antoine Corcy <dev@sbin.dk>
2014-03-15 14:03:05 +0100

    add --prefer-source flag

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -3,7 +3,7 @@
 if (!$loader = @require_once __DIR__ . '/../vendor/autoload.php') {
     die("You must set up the project dependencies, run the following commands:
 wget http://getcomposer.org/composer.phar
-php composer.phar install --dev
+php composer.phar install --dev --prefer-source
 ");
 }
 
