a34ea7a23ee2becdc6c50347c52f639feb6c10e7
Fred <frederes@free.fr>
2015-09-11 08:39:51 +0200

    Fix main class

--- Geocoder/LoggableGeocoder.php
+++ Geocoder/LoggableGeocoder.php
@@ -10,10 +10,10 @@
 
 namespace Bazinga\Bundle\GeocoderBundle\Geocoder;
 
-use Geocoder\TimedGeocoder;
+use Geocoder\ProviderAggregator;
 use Bazinga\Bundle\GeocoderBundle\Logger\GeocoderLogger;
 
-class LoggableGeocoder extends TimedGeocoder
+class LoggableGeocoder extends ProviderAggregator
 {
      /**
      * @var GeocoderLogger
