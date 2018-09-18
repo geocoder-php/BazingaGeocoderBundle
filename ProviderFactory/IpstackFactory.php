78e11999786062872ae8d5818ec3ea6f05e7a34a
Jonas Gielen <github@jonasgielen.be>
2018-09-18 07:15:48 +0200

    Add ipstack provider (#205)
    * Add ipstack provider
    
    * updated the list of all te providers
    
    * added functional test
    
    * added Ipstack to the ProviderFactoryTest
    
    * fix broken test

---
+++ ProviderFactory/IpstackFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the BazingaGeocoderBundle package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Bazinga\GeocoderBundle\ProviderFactory;
+
+use Geocoder\Provider\Provider;
+use Geocoder\Provider\Ipstack\Ipstack;
+use Http\Discovery\HttpClientDiscovery;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class IpstackFactory extends AbstractFactory
+{
+    protected static $dependencies = [
+        ['requiredClass' => Ipstack::class, 'packageName' => 'geocoder-php/ipstack-provider'],
+    ];
+
+    protected function getProvider(array $config): Provider
+    {
+        $httplug = $config['httplug_client'] ?: HttpClientDiscovery::find();
+
+        return new Ipstack($httplug, $config['api_key']);
+    }
+
+    protected static function configureOptionResolver(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'httplug_client' => null,
+        ]);
+
+        $resolver->setRequired('api_key');
+        $resolver->setAllowedTypes('httplug_client', ['object', 'null']);
+        $resolver->setAllowedTypes('api_key', ['string']);
+    }
+}
