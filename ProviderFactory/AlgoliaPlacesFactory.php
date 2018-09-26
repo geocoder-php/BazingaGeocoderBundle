3b20137e67dfc8b44e49a07d948f5131fd19381a
Fabien Lalanne <flalanne@enoptea.fr>
2018-09-26 16:00:41 +0200

    add Factory for Algolia Place Provider

---
+++ ProviderFactory/AlgoliaPlacesFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the BazingaGeocoderBundle package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Bazinga\GeocoderBundle\ProviderFactory;
+
+use Geocoder\Provider\AlgoliaPlaces\AlgoliaPlaces;
+use Geocoder\Provider\Provider;
+use Http\Discovery\HttpClientDiscovery;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class AlgoliaPlacesFactory extends AbstractFactory
+{
+    protected static $dependencies = [
+        ['requiredClass' => AlgoliaPlaces::class, 'packageName' => 'geocoder-php/algolia-place-provider'],
+    ];
+
+    protected function getProvider(array $config): Provider
+    {
+        $httplug = $config['httplug_client'] ?: HttpClientDiscovery::find();
+
+        return new AlgoliaPlaces($httplug, $config['apiKey'], $config['appId']);
+    }
+
+    protected static function configureOptionResolver(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'httplug_client' => null,
+            'apiKey' => null,
+            'appId' => null,
+        ]);
+
+        $resolver->setAllowedTypes('httplug_client', ['object', 'null']);
+        $resolver->setAllowedTypes('apiKey', ['string']);
+        $resolver->setAllowedTypes('appId', ['string']);
+    }
+}
