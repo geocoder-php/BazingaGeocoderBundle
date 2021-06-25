f378b05594abcf9856c60af97e855c0f9c32f0b6
Tomas <norkunas.tom@gmail.com>
2021-06-25 14:01:33 +0300

    Fix FakeIpPlugin

--- Tests/Plugin/FakeIpPluginTest.php
+++ Tests/Plugin/FakeIpPluginTest.php
@@ -35,7 +35,7 @@ class FakeIpPluginTest extends TestCase
 
     /**
      * @testWith [null]
-     *           ['']
+     *           [""]
      */
     public function testEmptyLocalIpQuery(?string $localIp)
     {
