38ff2fa89ef42a3b5b983ebb49b8f16fdf718b48
Christopher Georg <christopher.georg@sr-travel.de>
2025-12-22 18:58:36 +0100

    feat: remove MaxMindBinary Provider

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,6 +16,3 @@ wget http://getcomposer.org/composer.phar
 php composer.phar install --dev --prefer-source
 ');
 }
-
-require_once __DIR__.'/../vendor/geoip/geoip/src/geoip.inc';
-require_once __DIR__.'/../vendor/geoip/geoip/src/geoipcity.inc';
